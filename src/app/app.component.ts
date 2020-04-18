54acaf8b477c09c28201ee68c7d23105b5a5901b
Deni Simon <denisimon@denis-air.home>
2020-04-19 00:13:53 +0100

    added selection to recipe or shopping list based on selection from header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ang-shop-recipe';
+  showRecipe = true;
+
+  onRecipeOrShoppingSelection(recipeClicked: boolean) {
+    this.showRecipe = recipeClicked;
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  @Output() recipeClicked = new EventEmitter<boolean>();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  clickedOnRecipe() {
+    this.recipeClicked.emit(true);
+  }
+
+  clickedOnShoppingList() {
+    this.recipeClicked.emit(false);
+  }
 }
