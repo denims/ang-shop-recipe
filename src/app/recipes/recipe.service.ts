94abfd4f03e0544933547a91acf717194da4ee05
Deni Simon <denimallel@gmail.com>
2020-05-08 22:52:09 +0100

    Added ingredient as part of Recipe

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Injectable({
   providedIn: 'root'
@@ -7,8 +8,10 @@ import { Recipe } from './recipe.model';
 export class RecipeService {
   selectedRecipe = new EventEmitter<Recipe>();
   private recipe: Recipe[] =
-  [new Recipe('Test Recipe', 'Test desc', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg'),
-  new Recipe('Test Recipe 2', 'Test desc 2', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg')];
+  [new Recipe('Test Recipe', 'Test desc', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg',
+  [new Ingredient('Ingredient 1', 10), new Ingredient('Ingredient 2', 2)]),
+  new Recipe('Test Recipe 2', 'Test desc 2', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg',
+  [new Ingredient('Ingredient 3', 8), new Ingredient('Ingredient 4', 5)])];
 
   constructor() { }
 
